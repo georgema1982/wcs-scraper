ee5739d792ae1ff4bb8aaeb9452bfaeb62a4c67d
George Ma <george.ma1982@gmail.com>
2016-01-12 16:02:43 -0700

    Changed the scrapers and template based on the new layout

--- scrapewcs.py
+++ scrapewcs.py
@@ -5,8 +5,8 @@ from jinja2 import Environment, FileSystemLoader
 import requests
 
 q = pq(url = 'http://www.radioexpress.com/charts/world-chart/')
-title = q('div.entry-content strong')
-table = q('#tablepress-4')
+title = q('#primary p > strong > font')
+table = q('#tablepress-2')
 env = Environment(loader = FileSystemLoader('.'))
 with codecs.open(string.replace(title.text(), ' | ', ' ') + '.html', "w", "utf-8-sig") as output:
-    output.write(env.get_template('template.html').render(table_css = requests.get('http://www.radioexpress.com/wp-content/tablepress-combined.min.css?ver=10').text, title = title.outerHtml(), table = table.outerHtml()))
+    output.write(env.get_template('template.html').render(table_css = requests.get('http://www.radioexpress.com/wp-content/plugins/tablepress/css/default.min.css?ver=1.6.1').text, title = title.text(), table = table.outerHtml()))
